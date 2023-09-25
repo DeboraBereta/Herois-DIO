d537e5537a259968268070beffc26c1347a65b7a
debora bereta <108531081+DeboraBereta@users.noreply.github.com>
2023-09-25 09:06:22 -0300

    Add files via upload

--- Desafio.java
+++ Desafio.java
@@ -10,6 +10,7 @@ public class Desafio {
         System.out.println("Digite a Experiência (XP) do herói");
         int xp = Integer.parseInt(scanner.nextLine());
 
+        // Cria uma matriz bidimensional para armazenar intervalos de XP e suas respectivas mensagens.
         Object[][] xpHeroi = {
             {0, 1000, "Ferro"},
             {1001, 2000, "Bronze"},
@@ -21,14 +22,20 @@ public class Desafio {
             {10001, Integer.MAX_VALUE, "Radiante"}
         };
 
-        for (int i = 0; i < xpHeroi.length; i++) {
-            int min = (int) xpHeroi[i][0];
-            int max = (int) xpHeroi[i][1];
-            String mensagem = (String) xpHeroi[i][2];
+        // Verifica se a XP do herói está dentro do intervalo da primeira linha da matriz (0 a 1000).
+        if (xp >= (int) xpHeroi[0][0] && xp <= (int) xpHeroi[0][1]) {
+            System.out.println("O Herói " + nome + " está no nível de " + xpHeroi[0][2]);
+        } else {
+            // Se não estiver na primeira faixa, percorre o restante da matriz.
+            for (int i = 1; i < xpHeroi.length; i++) {
+                int min = (int) xpHeroi[i][0];
+                int max = (int) xpHeroi[i][1];
+                String mensagem = (String) xpHeroi[i][2];
 
-            if (xp >= min && xp <= max) {
-                System.out.println("O Herói " + nome + " está no nível de " + mensagem);
-                break; // Encerra o loop assim que a faixa de XP for encontrada
+                if (xp >= min && xp <= max) {
+                    System.out.println("O Herói " + nome + " está no nível de " + mensagem);
+                    break;
+                }
             }
         }
     }
